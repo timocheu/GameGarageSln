14fe490d92c6b1789232f7e049c7a598e75d992d
Papellero <ACT-STUDENT@LAB3-PC28>
2026-03-13 13:53:58 +0800

    Modify: Featured games

---
+++ GameGarage/Views/Home/_FeaturedGames.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GameGarage.Views.Home
+{
+    public class _FeaturedGamesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
